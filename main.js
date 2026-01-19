ba9a50de99d7ad35dc3fcee2275d60d4dbabc2f1
TheMcCain <shereen.hussian@gmail.com>
2026-01-19 12:31:52 -0500

    Add draggable window feature to main.js
    Implement draggable window functionality with mouse events.

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-(function () {
-    const hasPageScrollbar = document.documentElement.scrollHeight > document.documentElement.clientHeight;
-    document.body.classList.toggle('has-scrollbar', hasPageScrollbar);
-}
-)();
+const windowEl = document.querySelector(".window");
+const titleBar = windowEl.querySelector(".title-bar");
+
+let isDragging = false;
+let offsetX = 0;
+let offsetY = 0;
+
+titleBar.addEventListener("mousedown", (e) => {
+  isDragging = true;
+
+  const rect = windowEl.getBoundingClientRect();
+  offsetX = e.clientX - rect.left;
+  offsetY = e.clientY - rect.top;
+
+  document.body.style.userSelect = "none";
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (!isDragging) return;
+
+  windowEl.style.left = e.clientX - offsetX + "px";
+  windowEl.style.top = e.clientY - offsetY + "px";
+});
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+  document.body.style.userSelect = "";
+});
